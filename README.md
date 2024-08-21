4f4970208ee25cc219a8a9a6ad54a9aeb6d70925
XY0797 <98995022+XY0797@users.noreply.github.com>
2024-08-21 22:49:01 +0800

    v3.9

---
+++ README.md
@@ -0,0 +1,66 @@
+<h1 align="center">EQ-BDS面板 || 一站式BDS开服解决方案</h1>
+
+![图片](./EQ-BDSDtonsole.webp)
+
+<p align="center">
+  <img src="https://img.shields.io/github/v/release/XY0797/EQ-BDSDtonsole" />
+  <a href="https://github.com/XY0797/EQ-BDSDtonsole/blob/master/LICENSE">
+    <img alt="License: MPL-2.0" src="https://img.shields.io/github/license/XY0797/EQ-BDSDtonsole" target="_blank" />
+  </a>
+  <a href="https://jq.qq.com/?_wv=1027&k=jPV9ohWe">
+    <img src="https://img.shields.io/badge/QQ%E7%BE%A4-1072180746-green" target="_blank"/>
+  </a>
+</p>
+
+> #### 一个稳定、易用、扩展性强的Windows BDS面板
+> #### 专为BDS打造，集成了大量功能，采用分体式架构，稳定好用
+
+# 点这里看介绍👉[介绍页](https://xy0797.github.io/EQ-BDSDtonsoleDocs/)👈
+
+(介绍页有下载与食用方法)
+
++ #### MineBBS帖子：https://www.minebbs.com/resources/eq-bds-3-8.4289/
++ #### QQ群：👉[1072180746](https://jq.qq.com/?_wv=1027&k=jPV9ohWe)👈
+
+# 💾 项目引用
+
+### 1. 本项目的大量使用了[精易模块](http://ec.125.la/)封装的Win32API
+
+### 2. 本项目的网络通讯组件来自于[HP-Socket](https://github.com/ldcsaa/HP-Socket)
+
+### 3. 本项目使用了[meng9934](https://bbs.125.la/forum.php?mod=viewthread&tid=14691896)封装的多线程库(常用功能支持库2.0#2)
+
+### 4. 本项目使用了zyJson封装的json模块，用于处理json数据
+(注：未找到该作者的联系方式，只能在这里感谢该作者的默默付出)
+
+### 5. 本项目使用了[Mujs脚本引擎](https://www.mujs.com/)，本人封装为DLL并且由易语言调用
+
+# 🤝 贡献
+
+### 欢迎提出你的建议！
+
+### 如果有任何问题，欢迎到[issues page](https://github.com/kefranabg/readme-md-generator/issues)反馈或[加入我们的QQ群](https://jq.qq.com/?_wv=1027&k=jPV9ohWe)
+
+# 作者
+
+## 👤 **XY0797**
+
+- ### QQ号：1559095867
+
+- ### e-mail：1559095867@qq.com
+
+# 支持我们
+
+### 如果您觉得本项目对您有用的话，请⭐️本代码仓库
+
+### Please ⭐️ this repository if this project helped you!
+
+# 📝 开源协议
+
+Copyright © 2023 [XY0797](https://github.com/XY0797)
+
+鉴于易语言的特殊性,EQ面板不会全部开源，否则可能被黑产利用
+
+仅开源EQ面板除了加密算法、面板主程序和QQ机器人插件之外的部分
+
+本仓库代码以[MPL-2.0](https://github.com/XY0797/EQ-BDSDtonsole/blob/master/LICENSE)协议开源
---
+++ mujslib/mujsDLL/dllmain.cpp
@@ -0,0 +1,123 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+//GB2312到UTF-8的转换
+static char* GB2312ToUtf8(const char* gb2312)//记得释放内存！！
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* utf8 = new char[len + 1];
+	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, utf8, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return utf8;
+}
+//UTF-8到GB2312的转换
+static char* Utf8ToGB2312(const char* utf8)//记得释放内存！！
+{
+	int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+	len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* gb2312 = new char[len + 1];
+	WideCharToMultiByte(CP_ACP, 0, wstr, -1, gb2312, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return gb2312;
+}
+
+
+//创建js对象，返回指针
+//panicFunction要求一个参数接收js对象指针
+//reportFunction要求两个参数，第一个接收指针，第二个接收错误信息
+js_State* WINAPI CreatNewJsObj(js_Panic panicFunction, js_Report reportFunction) {
+	js_State* ptr = js_newstate(NULL, NULL, JS_STRICT);
+	js_atpanic(ptr,panicFunction);
+	js_setreport(ptr, reportFunction);
+	return ptr;
+}
+
+//销毁js对象
+void WINAPI DestroyJsObj(js_State* jsObjPointer) {
+	js_freestate(jsObjPointer);
+}
+//注册js函数，函数指针必须有一个参数接收js对象
+void WINAPI RegFunction(js_State* jsObjPointer, js_CPPFunction FunctionPointer, char* FunctionNameInJs, long ArgumentNumber) {
+	char* UTF8FunctionNameInJs=GB2312ToUtf8(FunctionNameInJs);
+	js_newcppfunction(jsObjPointer, FunctionPointer, UTF8FunctionNameInJs, ArgumentNumber);
+	js_setglobal(jsObjPointer, UTF8FunctionNameInJs);
+	delete[] UTF8FunctionNameInJs;
+}
+
+//执行js代码
+void WINAPI ExecuteJsCode(js_State* jsObjPointer, char* JsCode) {
+	char* UTF8JsCode = GB2312ToUtf8(JsCode);
+	js_dostring(jsObjPointer, UTF8JsCode);
+	delete[] UTF8JsCode;
+}
+
+
+
+//函数返回null型
+void WINAPI js_returnNull(js_State* J) {
+	js_pushnull(J);
+}
+
+//函数返回易语言逻辑型
+void WINAPI js_returnBoolean(js_State* J, int v) {
+	js_pushboolean(J, v);
+}
+
+//函数返回易语言双精度小数型
+void WINAPI js_returnNumber(js_State* J, double v) {
+	js_pushnumber(J, v);
+}
+
+//函数返回易语言文本型，注意指定长度
+void WINAPI js_returnlString(js_State* J, char* v) {
+	int len = MultiByteToWideChar(CP_ACP, 0, v, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_ACP, 0, v, -1, wstr, len);
+	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* utf8 = new char[len + 1];
+	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, utf8, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	js_pushlstring(J, utf8, len);
+	delete[] utf8;
+}
+
+
+
+//易语言逻辑型
+int WINAPI js_getBoolean(js_State* J, int idx) {
+	return js_toboolean(J, idx);
+}
+
+//易语言双精度小数型
+double WINAPI js_getNumber(js_State* J, int idx) {
+	return js_tonumber(J, idx);
+}
+
+//易语言整数型
+int WINAPI js_getInt32(js_State* J, int idx) {
+	return js_toint32(J, idx);
+}
+
+//易语言短整数型
+short WINAPI js_getInt16(js_State* J, int idx) {
+	return js_toint16(J, idx);
+}
+
+//易语言文本型
+const char* WINAPI js_getString(js_State* J, int idx) {
+	char* GB2312Str = Utf8ToGB2312(js_tostring(J, idx));
+	return GB2312Str;
+	//return js_tostring(J, idx);
+}
+
+//释放文本型指针
+void WINAPI deleteString(char* lpStr) {
+	delete[] lpStr;
+}
